37679289d4cbe20a58e9996b88a0ac9bb75e6547
jiyeonCoder <jiyeon.choi218@gmail.com>
2023-09-06 17:08:43 +0900

    Applying Read

--- todo/urls.py
+++ todo/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('index/', views.index),
+    path('', views.index),
     path('create/', views.create),
+    path('<int:todo_id>/', views.read),
 ]
--- todo/views.py
+++ todo/views.py
@@ -4,8 +4,14 @@ from todo.models import Todo
 
 
 def index(request):
-    # name = request.GET.get('name')
-    return render(request, 'todo/index.html')
+    if request.method == 'GET':
+        todos = Todo.objects.all()
+        context = {
+            'todos': todos,
+        }
+        return render(request, 'todo/index.html', context)
+    else:
+        return HttpResponse('Invalid request method', status=405)
 
 
 def create(request):
@@ -16,3 +22,11 @@ def create(request):
         return render(request, 'todo/create.html')
     else:
         return HttpResponse('Invalid request method', status=405)
+
+
+def read(request, todo_id):
+    todo = Todo.objects.get(id=todo_id)
+    context = {
+        'todo': todo,
+    }
+    return render(request, 'todo/detail.html', context)
